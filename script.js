e9534ea9e60de524c1abc873303b19e0d093a7ed
huy2000vu <huy2000vu@gmail.com>
2023-08-11 15:13:20 -0700

    create a funct that generate a random number for computer

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello");
+const rock = 1;
+const paper = 2;
+const scissor = 3;
+function getComputerChoice() {
+  let randomNum = Math.round(Math.random() * 2) + 1; //generate a random number from 1-3
+  switch (randomNum) {
+    case 1:
+      return "rock";
+      break;
+    case 2:
+      return "paper";
+      break;
+    case 3:
+      return "scissor";
+  }
+}
