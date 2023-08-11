b1731467aa321a48dcd3145ffb484925874c4f23
huy2000vu <huy2000vu@gmail.com>
2023-08-11 15:22:59 -0700

    Make player script

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const rock = 1;
-const paper = 2;
-const scissor = 3;
 function getComputerChoice() {
   let randomNum = Math.round(Math.random() * 2) + 1; //generate a random number from 1-3
   switch (randomNum) {
@@ -13,4 +10,7 @@ function getComputerChoice() {
     case 3:
       return "scissor";
   }
+}
+for (let i = 0; i <= 10; i++) {
+  console.log(getComputerChoice());
 }
