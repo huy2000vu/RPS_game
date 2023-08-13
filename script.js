cc4f32bb4b11d905753feb817151597e774e402e
huy2000vu <huy2000vu@gmail.com>
2023-08-12 18:23:57 -0700

    final touches on game funct

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let pcPoints = 0;
+let playerPoints = 0;
+//function that assigns pc a choice
 function getComputerChoice() {
   let randomNum = Math.round(Math.random() * 2) + 1; //generate a random number from 1-3
   switch (randomNum) {
@@ -11,6 +14,7 @@ function getComputerChoice() {
       return "scissor";
   }
 }
+//evaluate player choice vs pc choice
 function playRound(pc, player) {
   if (pc.toLowerCase() === player.toLowerCase()) {
     return "tie";
@@ -19,11 +23,32 @@ function playRound(pc, player) {
     (pc.toLowerCase() === "paper" && player.toLowerCase() === "rock") ||
     (pc.toLowerCase() === "scissor" && player.toLowerCase() === "paper")
   ) {
+    pcPoints++;
     return "pc wins";
-  } else return "player wins";
+  } else {
+    playerPoints++;
+    return "player wins";
+  }
 }
-const pcChoice = getComputerChoice();
-const playerChoice = "rock";
-console.log("pc: " + pcChoice);
-console.log("player: " + playerChoice);
-console.log(playRound(pcChoice, playerChoice));
+//test cases
+let pcChoice; // = getComputerChoice();
+let playerChoice; // = prompt("enter rock paper or scissor: ");
+//function that runs games
+function game() {
+  for (let i = 1; i <= 5; i++) {
+    pcChoice = getComputerChoice();
+    playerChoice = prompt("enter rock paper or scissor: ", "");
+    console.log(`Round ${i}: PC->${pcChoice} Player->${playerChoice}`);
+    console.log(`${playRound(pcChoice, playerChoice)}`);
+  }
+  //display points and winner of the game
+  console.log(`PC points: ${pcPoints} Player points: ${playerPoints}`);
+  if (pcPoints > playerPoints) {
+    console.log("PC WIN");
+  } else if (playerPoints > pcPoints) console.log("YOU WIN");
+  else console.log("tie");
+  //reset points
+  pcPoints = 0;
+  playerPoints = 0;
+}
+game();
